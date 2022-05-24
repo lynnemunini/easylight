63d403b2e98ed6a29345365e655f90899bdad9de
lynnemunini <muninilynne65@gmail.com>
2022-05-25 00:31:12 +0300

    Created a login screen

---
+++ lib/signup.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+//import 'package:google_fonts/google_fonts.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // static const String _title = 'Sample App';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: FlexThemeData.light(
+        scheme: FlexScheme.aquaBlue,
+        //textTheme: GoogleFonts.poppinsTextTheme()
+      ),
+      /*The Mandy red, dark theme.
+      darkTheme: FlexThemeData.dark(scheme: FlexScheme.aquaBlue),
+      Use dark or light theme based on system setting.
+      themeMode: ThemeMode.system,*/
+      home: Scaffold(
+        appBar: AppBar(),
+        body: const MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  // Initially password is obscure
+  bool _isObscure = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Create account',
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 35),
+                )),
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(30),
+                child: const Text(
+                  'Sign up',
+                  style: TextStyle(fontSize: 20),
+                )),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Phone Number',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email Address',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Meter Number',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
+              child: TextField(
+                obscureText: _isObscure,
+                controller: passwordController,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  labelText: 'Password',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isObscure ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isObscure = !_isObscure;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ),
+
+            // Confirm Password
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
+              child: TextField(
+                obscureText: _isObscure,
+                controller: passwordController,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  labelText: 'Confirm Password',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isObscure ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isObscure = !_isObscure;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ),
+            Container(
+                height: 70,
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                child: ElevatedButton(
+                  child: const Text('Register'),
+                  onPressed: () {
+                    print(nameController.text);
+                    print(passwordController.text);
+                  },
+                )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('Already have an account?'),
+                TextButton(
+                  child: const Text(
+                    'Sign in',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  onPressed: () {
+                    //login screen
+                  },
+                )
+              ],
+            ),
+          ],
+        ));
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:login_screen/main.dart';
+import 'package:login_screen/login.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
