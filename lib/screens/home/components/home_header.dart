fcbba2e59dccf84eba34e957d57f41bf30bd665c
lynnemunini <muninilynne65@gmail.com>
2022-05-28 00:40:49 +0300

    Wdited header

--- lib/screens/home/components/home_header.dart
+++ lib/screens/home/components/home_header.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:token_app/screens/profile/profile_screen.dart';
 
+import '../../../constants.dart';
 import '../../../size_config.dart';
 
 class HomeHeader extends StatelessWidget {
@@ -14,7 +17,32 @@ class HomeHeader extends StatelessWidget {
           EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [],
+        children: [
+          IconButton(
+            icon: SvgPicture.asset(
+              "assets/icons/menu.svg",
+              color: kPrimaryColor,
+            ),
+            onPressed: () =>
+                Navigator.pushNamed(context, ProfileScreen.routeName),
+          ),
+          Text(
+            "Hello, Lynne",
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                //color: kPrimaryColor,
+                fontSize: 16),
+          ),
+          IconButton(
+            icon: SvgPicture.asset(
+              "assets/icons/User Icon.svg",
+              color: kPrimaryColor,
+            ),
+            onPressed: () =>
+                Navigator.pushNamed(context, ProfileScreen.routeName),
+          ),
+        ],
       ),
     );
   }
