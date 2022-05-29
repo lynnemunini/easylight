7171b41a748f45f66a9048759d9761f78c423d9d
lynnemunini <muninilynne65@gmail.com>
2022-05-29 11:02:53 +0300

    Initialized real time database

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,41 @@ import 'package:token_app/routes.dart';
 import 'screens/splash/splash_screen.dart';
 import 'package:token_app/theme.dart';
 
-void main() {
+//Import firebase core
+import 'package:firebase_core/firebase_core.dart';
+
+//Import real time database package
+import 'package:firebase_database/firebase_database.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: theme(),
-      home: SplashScreen(),
+      home: FutureBuilder(
+        future: _fbApp,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print(snapshot.error);
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return SplashScreen();
+          }
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        },
+      ),
       // We use routeName so that we dont need to remember the name
       initialRoute: SplashScreen.routeName,
       routes: routes,
