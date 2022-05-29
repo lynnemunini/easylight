8944f9a6860746593222700de6d22fa2be0ad26c
lynnemunini <muninilynne65@gmail.com>
2022-05-29 15:20:42 +0300

    Removed firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,40 +3,19 @@ import 'package:token_app/routes.dart';
 import 'screens/splash/splash_screen.dart';
 import 'package:token_app/theme.dart';
 
-//Import firebase core
-import 'package:firebase_core/firebase_core.dart';
-//Import real time database package
-import 'package:firebase_database/firebase_database.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       theme: theme(),
-      home: FutureBuilder(
-        future: _fbApp,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            print(snapshot.error);
-          }
-          if (snapshot.connectionState == ConnectionState.done) {
-            return SplashScreen();
-          }
-          return Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        },
-      ),
+      // home: SplashScreen(),
       // We use routeName so that we dont need to remember the name
       initialRoute: SplashScreen.routeName,
       routes: routes,
